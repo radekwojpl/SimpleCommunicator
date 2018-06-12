445d50811cb27839c2e006a3937a40a0843d63f1
radekwojpl <radekbajor27@gmail.com>
2018-06-12 09:25:43 +0200

    init

---
+++ SimpleCommunicator/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SimpleCommunicator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var viewModel = new SimpleCommunicationViewModel();
+
+            MainWindow window = new MainWindow( viewModel);
+
+            window.Show();
+
+
+        }
+    }
+}
