d24e7b0d13916f7f0d21b33fdfac3dba191bbdd8
radekwojpl <radekbajor27@gmail.com>
2018-08-02 21:16:58 +0200

    MainWindow, Communicator, LoginScreen

--- SimpleCommunicator/ViewModel/SimpleCom/SimpleCommunicationViewModel.cs
+++ SimpleCommunicator/ViewModel/SimpleCom/SimpleCommunicationViewModel.cs
@@ -5,22 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
+using SimpleCommunicator.Views;
+using SimpleCommunicator.Views.LoginScreen;
 
 namespace SimpleCommunicator
 {
     public class SimpleCommunicationViewModel : ViewModelBase, INotifyPropertyChanged
     {
-     
-        
+        private ContentControl currentContext = new ContentControl();
+
+        public ContentControl CurrentContext
+        {
+            get { return currentContext; }
+            set { currentContext = value; RaisePropertyChanged("CurrentContext"); }
+        }
 
         public SimpleCommunicationViewModel() => SetUp();
 
         public void SetUp()
         {
-           
+            currentContext.Content = new LoginScreen();
         }
 
       
